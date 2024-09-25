737b48a7d1ced3a422b8b763f3efef23b9daecac
Pranay Raut <pranayraut129@gmail.com>
2024-09-26 00:54:00 +0530

    Add project files.

---
+++ PalindromeNumber/Models/CheckPalindrome.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeNumber.Models
+{
+    internal class CheckPalindrome
+    {
+        public static bool ToCheckPalindrome(int number)
+        {
+            int tempValue = number;
+            int Reverse = 0;
+            while (tempValue > 0)
+            {
+                int lastNumber = tempValue % 10;
+                Reverse = (Reverse * 10) + lastNumber;
+                tempValue = tempValue / 10;
+            }
+            return Reverse == number;
+        }
+
+    }
+}
---
+++ PalindromeNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿using PalindromeNumber.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Number");
+            int number = int.Parse(Console.ReadLine());
+            if (CheckPalindrome.ToCheckPalindrome(number))
+            {
+                Console.WriteLine($"The Number {number} is Palindrome");
+            }
+            else
+            {
+                Console.WriteLine($"The Number {number} is NOt a Palindome");
+            }
+            
+        }
+    }
+}
